d0341425a00fef900980e94e16737e51169045bd
Lorenzo-Perini <lorenzo.perini@kuleuven.be>
2023-02-23 17:50:31 +0100

    Upload 23_02_23

--- BALLAD.py
+++ BALLAD.py
@@ -3,11 +3,82 @@ from skopt import gp_minimize
 from scipy.spatial.distance import cosine
 import matplotlib.pyplot as plt
 import pandas as pd
-from src.anomatools.models import SSDO
+from anomatools.models import SSDO
 from scipy.io import arff
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons
+import seaborn as sns
+from anomatools.models.scalers import SquashScaler
+from anomatools.models._knno import kNNO
+
+def run_BALLAD(X_train, y_train, X_val, y_val, X_test, y_test, contamination, metric, tot_budget, pool_budget, 
+               c_r, c_fp, c_fn, seed=331, plots = False, n_jobs = -1):
+    
+    indicesRanked = np.array([])
+    used_budget = 0
+    ALreward = .99
+    LRreward = 1
+    y_train_semitargets = np.zeros(len(y_train), np.int)
+    y_val_semitargets = np.zeros(len(y_val), np.int)
+
+    oldpred_AL, oldpred_LR = initialize_loop(np.concatenate((X_train, X_val)), seed, c_r, c_fp, c_fn, metric, contamination)
+
+    while used_budget < tot_budget:
+        pool_budget = min(pool_budget, tot_budget-used_budget)
+        detector = SSDO(base_detector = kNNO(scaler=SquashScaler(contamination=contamination)),
+                        scaler=SquashScaler(contamination=contamination))
+        #Add labels
+        if ALreward >= LRreward:
+            if used_budget>pool_budget:
+                print("Budget used:", used_budget+pool_budget,"out of:", tot_budget,
+                      "--> Current Chosen Strategy: ___ Active Learning ___")
+
+            y_train_semitargets = add_training_labels(y_train_semitargets, y_train, indicesRanked, pool_budget)
+            detector.fit(X_train, y_train_semitargets)
+            labeld_idx_val = np.where(y_val_semitargets !=0)[0]
+            if len(labeld_idx_val) > 0:
+                val_probs = detector.predict_proba(X_val[labeld_idx_val])[:,1]
+                rejection_threshold = set_rejection_threshold(val_probs, y_val[labeld_idx_val], c_r, c_fp, c_fn, n_jobs)
+            else:
+                rejection_threshold = 0.0
+
+            predictions = detector.predict(np.concatenate((X_train, X_val)))
+            probs = detector.predict_proba(np.concatenate((X_train, X_val)))[:,1]
+
+            ALreward, _, oldpred_AL, _ = measure_rewards(predictions, probs, rejection_threshold, 
+                                                         oldpred_AL, oldpred_LR, metric = metric)
+
+            al_scores = 2*abs(detector.predict_proba(X_train)[:,1] - 0.5)
+            indicesRanked = np.argsort(al_scores)
+
+        else:
+            if used_budget>pool_budget:
+                print("Budget used:", used_budget+pool_budget,"out of:", tot_budget,
+                      "--> Current Chosen Strategy: ___ Learning to Reject ___")
+            y_val_semitargets = add_validation_labels(y_val_semitargets, y_val, pool_budget)
+            detector.fit(X_train, y_train_semitargets)
+            labeled_idx_val = np.where(y_val_semitargets !=0)[0]
+            val_probs = detector.predict_proba(X_val[labeled_idx_val])[:,1]
+            rejection_threshold = set_rejection_threshold(val_probs, y_val[labeled_idx_val], c_r, c_fp, c_fn, n_jobs)
+            predictions = detector.predict(np.concatenate((X_train, X_val)))
+            probs = detector.predict_proba(np.concatenate((X_train, X_val)))[:,1]
+            _, LRreward, _, oldpred_LR = measure_rewards(predictions, probs, rejection_threshold, 
+                                                         oldpred_AL, oldpred_LR, metric = metric)
+
+        test_pred = detector.predict(X_test)
+        test_prob = detector.predict_proba(X_test)[:,1]
+        if plots:
+            plot_BALLAD(X_train, y_train, detector, y_train_semitargets, rejection_threshold)
+        
+        reject_indx = reject(test_pred, test_prob, rejection_threshold)
+        test_pred[reject_indx] = 2
+        test_cost_Rej, fpr, fnr, rr = cost_function(y_test, test_pred, c_r, c_fp, c_fn)
+        used_budget+=pool_budget
+        if used_budget>2*pool_budget:
+            print("Test Cost using BALLAD:", np.round(test_cost_Rej,5))
+    return
+
 
 def load_dataset(dataset_name, path, random_state):
     dslist = ['ALOI_withoutdupl_norm.arff',
@@ -86,8 +157,9 @@ def load_dataset(dataset_name, path, random_state):
     return X_train_scaled, y_train, X_val_scaled, y_val, X_test_scaled, y_test, contamination
 
 
-def initialize_loop(X_train, contamination, random_state, c_r, c_fp, c_fn, metric):
-    detector = SSDO()
+def initialize_loop(X_train, random_state, c_r, c_fp, c_fn, metric, contamination):
+    detector = SSDO(base_detector = kNNO(scaler=SquashScaler(contamination=contamination)),
+                    scaler=SquashScaler(contamination=contamination))
     detector.fit(X_train, np.zeros(len(X_train), np.int))
     
     if metric == 'cosine':
@@ -110,7 +182,7 @@ def add_validation_labels(y_val_semitargets, y_val, budget):
     y_val_semitargets[indeces] = y_val[indeces]
     return y_val_semitargets
 
-def set_rejection_threshold(val_probs, y_val, c_r, c_fp, c_fn):
+def set_rejection_threshold(val_probs, y_val, c_r, c_fp, c_fn, n_jobs):
     if len(val_probs) == 0:
         rejection_threshold = 0.0
         print("##### NO VALIDATION LABELS. SETTING THE REJECTION THRESHOLD TO 0.")
@@ -121,7 +193,7 @@ def set_rejection_threshold(val_probs, y_val, c_r, c_fp, c_fn):
         if min_quantile == max_quantile:
             min_quantile = max(0, max_quantile - 0.1)
             max_quantile = min(1, max_quantile +0.1)
-        predictions = np.where(val_probs >= 0.5, 1, 0)
+        predictions = np.where(val_probs >= 0.5, 1, -1)
     
     def optimize_cost_function(thr):
         non_rejected = np.where(rej_scores >= thr)[0]
@@ -135,8 +207,8 @@ def set_rejection_threshold(val_probs, y_val, c_r, c_fp, c_fn):
         cost = (false_positives*c_fp + false_negatives*c_fn + (len(y_val) - len(non_rejected)) *c_r)/len(y_val)
         return cost
     
-    res = gp_minimize(func = optimize_cost_function, dimensions = [(min_quantile,max_quantile)], n_calls = 20,
-                      random_state = 331, x0 = [np.quantile(rej_scores, 0.10)])
+    res = gp_minimize(func = optimize_cost_function, dimensions = [(min_quantile,max_quantile)], n_calls = 50,
+                      random_state = 331, x0 = [np.quantile(rej_scores, 0.10)], n_jobs = n_jobs, verbose = False)
     rejection_threshold = res.x
     return rejection_threshold[0]
 
@@ -185,3 +257,63 @@ def add_training_labels(y_train_semitargets, y_train, rankingAL, poolBudget):
 
     return y_train_semitargets
 
+def plot_BALLAD(X_train, y_train, detector, y_train_semitargets, rejection_threshold):
+    import matplotlib as mtl
+    margin_size = 0.1
+    steps = 100
+
+    x_min, x_max = X_train[:, 0].min(), X_train[:, 0].max()
+    y_min, y_max = X_train[:, 1].min(), X_train[:, 1].max()
+    x_range = abs(x_max - x_min)
+    y_range = abs(y_max - y_min)
+
+    xmin, xmax = x_min - margin_size * x_range, x_max + margin_size * x_range
+    ymin, ymax = y_min - margin_size * y_range, y_max + margin_size * y_range
+
+    # make the meshgrid based on the data 
+    xx, yy = np.meshgrid(np.linspace(xmin, xmax, int(steps)), np.linspace(ymin, ymax, int(steps)))
+    X_mesh = np.c_[xx.ravel(), yy.ravel()]
+
+    fig = plt.figure(figsize=(10,8), facecolor='w', edgecolor='k')
+    pos_probs_background = detector.predict_proba(X_mesh)[:,1]
+    reject_probs = 2*abs(pos_probs_background - 0.5)
+
+    #scores = clf.predict_proba(X_mesh)[:,1]
+    pos_probs_background = pos_probs_background.reshape(xx.shape)
+    plt.contourf(xx, yy, pos_probs_background, cmap=plt.cm.Reds, norm=mtl.colors.Normalize(vmin=0, vmax=1), alpha=0.8)
+    Z = detector.predict(X_mesh)
+    Z = Z.reshape(xx.shape)
+    plt.contour(xx, yy, Z, cmap=plt.cm.Greens, alpha = 1)
+
+    reject_probs = reject_probs.reshape(xx.shape)
+    plt.contourf(xx, yy, reject_probs, levels = [0, rejection_threshold], colors = 'darkblue')
+
+    ax, _ = mtl.colorbar.make_axes(plt.gca(), shrink=1.0)
+    cbar = mtl.colorbar.ColorbarBase(ax,cmap=plt.cm.Reds,norm=mtl.colors.Normalize(vmin=0,vmax=1),
+                                     ticks = np.arange(0,105,20)/100)
+    plt.subplot(1,1,1)
+    NewDtrain = X_train[[x for x in range(len(X_train)) if y_train_semitargets[x]==0]]
+    newy = y_train[[x for x in range(len(X_train)) if y_train_semitargets[x]==0]]
+    class1pred = NewDtrain[np.where(newy == 1)]
+    class0pred = NewDtrain[np.where(newy == -1)]
+    cl0 = plt.scatter(class0pred.T[0],class0pred.T[1], 400, c = 'gray', alpha = 0.7, edgecolor = 'black',
+                      label = 'Class 0')
+    cl1 = plt.scatter(class1pred.T[0],class1pred.T[1], 450, c = 'gray', marker = '^',edgecolor = 'black',
+                      alpha = 0.7, label = 'Class 1')
+
+    us = np.where(y_train_semitargets!=0)[0]
+    plt.scatter(X_train[us].T[0],X_train[us].T[1], 400, c = 'darkgreen', alpha = 1, edgecolor = 'black',
+                marker = 'X', clip_on = True, zorder = 1)
+
+    plt.legend((cl0, cl1),#, cl2
+               ('Normal', 'Anomaly'),
+               scatterpoints=1,
+               loc='lower left',
+               #bbox_to_anchor=(-.54, +0.1),
+               ncol=2,
+               fontsize=21,frameon=True).get_frame().set_edgecolor('black')
+    plt.axis('off')
+    sns.despine(right = True)
+    #plt.savefig('ex_3.jpg', format = 'jpg', bbox_inches = 'tight', dpi=1200)
+    plt.show()
+    return
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Ballad
-Implementation of the method BALLAD, published at the workshop UDM23 @ AAAI.
+# BALLAD
+
+`BALLAD` (Budget allocation for Active Learning and Learning to reject in Anomaly Detection) is a GitHub repository containing the **BALLAD** [1] algorithm.
+It refers to the paper titled *How to Allocate your Label Budget? Choosing between Active Learning and Learning to Reject in Anomaly Detection*, presented at the 1st AAAI Workshop on Uncertainty Reasoning and Quantification in Decision Making on February 13th 2023 in Washington D.C.
+
+Read the ArXiv version here: [[pdf](https://arxiv.org/pdf/2301.02909.pdf)].
+
+## Abstract
+
+Anomaly detection attempts at finding examples that deviate from the expected behaviour. Usually, anomaly detection is tackled from an unsupervised perspective because anomalous labels are rare and difficult to acquire. However, the lack of labels makes the anomaly detector have high uncertainty in some regions, which usually results in poor predictive performance or low user trust in the predictions. 
+One can reduce such uncertainty by collecting specific labels using Active Learning (AL), which targets examples close to the detector’s decision boundary. Alternatively, one can increase the user trust by allowing the detector to abstain from making highly uncertain predictions, which is called Learning to Reject (LR). One way to do this is by thresholding the detector’s uncertainty based on where its performance is low, which requires labels to be evaluated. Although both AL and LR need labels, they work with different types of labels: AL seeks strategic labels, which are evidently biased, while LR requires i.i.d. labels to evaluate the detector’s performance and set the rejection threshold. Because one usually has a unique label budget, deciding how to optimally allocate it is challenging. 
+In this paper, we propose a mixed strategy that, given a budget of labels, decides in multiple rounds whether to use the budget to collect AL labels or LR labels. The strategy is based on a reward function that measures the expected gain when allocating the budget to either side. We evaluate our strategy on 18 benchmark datasets and compare it to some baselines.
+
+## Contents and usage
+
+The repository contains:
+- BALLAD.py, a set of functions that allow to run our method BALLAD to smartly allocate your label budget;
+- Notebook.ipynb, a notebook showing how to use BALLAD on an artificial 2D dataset;
+
+To use BALLAD, import the github repository or simply download the files. You can also find the benchmark datasets at this [[link](https://www.dbs.ifi.lmu.de/research/outlier-evaluation/DAMI/)]. Please check the dataset's names inside the function "load_dataset" in BALLAD.py to replicate our experiments.
+
+
+## Budget allocation for Active Learning and Learning to reject in Anomaly Detection (BALLAD)
+
+Given a dataset with attributes **X**, we use SSDO [[paper](https://ieeexplore.ieee.org/document/8594877)] as a semi-supervised anomaly detector to learn from partially labeled examples. We simulate k allocation rounds. We initialize the problem by (1) training SSDO with no labels and setting a default rejection threshold to 0.1, and (2) collecting random labels for LR and for AL in the first two allocation rounds. This allows us to compute the initial rewards by **measuring how the detector varies from (1) to (2)**: for LR, we measure the variation after re-setting the validation threshold; for AL, we measure the variation after re-training the detector with the new labels. Then, we start the allocation loop. In each round, we allocate the budget to the option (LR or AL) with the **highest reward**, and we update the reward using the new labels. We propose two alternative reward functions: 1) the **entropy reward** looks at the detector’s probabilities, either for prediction (AL), or for rejection (LR); 2) the **cosine reward** considers the predicted class labels, either anomaly yes/no (AL), or rejection yes/no (LR).
+
+Given a dataset **X** with true labels **y**, we first split it into training/validation/test using the proportion 40/40/20. You can download the benchmark datasets and specify their path and their name (or set them as empty strings to use our 2D toy dataset):
+```python
+from BALLAD import load_dataset
+X_train, y_train, X_val, y_val, X_test, y_test, contamination = load_dataset(dataset_name, data_path, random_state = 331)
+```
+
+Then, our algorithm is applied as follows:
+
+```python
+from BALLAD import *
+run_BALLAD(X_train, y_train, X_val, y_val, X_test, y_test, contamination, metric, tot_budget, pool_budget, c_r, c_fp, c_fn, seed, plots)
+
+# contamination = proportion of anomalies in the training set [[see here](https://arxiv.org/pdf/2210.10487.pdf)], metric = 'cosine' or 'entropy'
+# tot_budget is the integer number of total labels, while pool_budget is the integer number of labels per allocation round
+# c_r, c_fp, c_fn are the costs for rejection, false positives and false negatives
+# seed allows replicability, and plots is a boolean value to get the heat-map of the model in each allocation round
+```
+
+## Dependencies
+
+The `run_BALLAD` function requires the following python packages to be used:
+- [Python 3](http://www.python.org)
+- [Numpy](http://www.numpy.org)
+- [Scipy](http://www.scipy.org)
+- [Pandas](https://pandas.pydata.org/)
+- [Anomatools](https://github.com/Vincent-Vercruyssen/anomatools)
+
+
+## Contact
+
+Contact the author of the paper: [lorenzo.perini@kuleuven.be](mailto:lorenzo.perini@kuleuven.be).
+
+
+## References
+
+[1] Perini, L., Giannuzzi, D. and Davis, J., 2023: *How to Allocate your Label Budget? Choosing between Active Learning and Learning to Reject in Anomaly Detection*, arXiv preprint arXiv:2301.02909.
